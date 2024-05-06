0f8302b26f42a599d7d73a7995821ab4d8f6f5c3
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-06 18:25:12 +0530

    mini e-commerce website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ProductDetails from './pages/ProductDetails';
+import {useState} from 'react';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <div>
+          <Header
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+          />
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/search'
+              element={<Home />}
+            />
+            <Route
+              path='/product/:id'
+              element={<ProductDetails />}
+            />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
     </div>
   );
 }
